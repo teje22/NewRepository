47365b85545f49cfb21e5b4c126a84e10031d6cb
AzureAD\PabloTejedorRivadull <2021360@iesluisbraille.es>
2022-01-27 09:53:18 +0100

    Version 2.0

--- EuroMillon.py
+++ EuroMillon.py
@@ -38,12 +38,26 @@ print("Bienvenidos al sorteo de la loteria del EuroMillon del 3 de Diciembre de 
 
 print("\n Para participar debe elegir 5 numeros de los primeros y 2 mas de la segunda")
 
+for i in listaNums:
+    print(*i)
 
-
-
-
-
-
+        
+while len(miBoletoAuxNum)<5: #Hacemos con un while diciendole que mientras la longitud de la lista "miBoletoAuxNum" sea menor que 5 nos pida nos pida otro numero
+    boleto=int(input("Elija 5 numeros: ")) #Creamos una variable que sea donde se guardan los valores que introducimos para que se vayan introduciendo en la lista
+    if boleto not in miBoletoAuxNum and boleto >=1 and boleto <=50: #Le ponemos la condicion al bucle while de que si el numero que introducimos el cual se encuentra guardado en "boleto" no esta ya en la lista "miBoletoAuxNum" y esta comprendido entre los numeros 1 y 50 entonces pase al siguiente paso que es introducirlo en la lista 
+        miBoletoAuxNum.append(boleto) #Hacemos que si cumple la condicion anterior se incluya el numero en la lista
+    else:
+        print("Número no valido o repetido ") #Si no se cumple las condiciones de antes entonces sale un mensaje por pantalla de numero no valido y no se introduce en la lista
+        
+# Creamos un bucle para que se vaya recorriendo nuestra lista de listas y cuando encuentre los números repetidos de nuestro boleto en la lista de todos los numeros y los cambie por una X
+for l1 in listaNums:
+    for numBoleto in miBoletoAuxNum:
+            cont=0
+            for e in l1:
+                if(l1[cont]==numBoleto):
+                    l1[cont]=" X"
+                cont+=1
+print("Los números que ha elegido se han borrado de su lista")
 
 
 
