6b0c7d332a3d06ad4974584572a4379664a675f2
AzureAD\PabloTejedorRivadull <2021360@iesluisbraille.es>
2022-01-27 09:42:54 +0100

    Version 1.0

---
+++ EuroMillon.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+@author: DavidCamposSerrano y Pablo Tejedor Rivadulla
+"""
+
+import random
+
+listaNums=[[1,10,19,28,37,46],
+           [2,11,20,29,38,47],
+           [3,12,21,30,39,48],
+           [4,13,22,31,40,49],
+           [5,14,23,32,41,50],
+           [6,15,24,33,42],
+           [7,16,25,34,43],
+           [8,17,26,35,44],
+           [9,18,27,36,45],
+           ]
+
+listaEstrellas=[[1,5,9],
+                [2,6,10],
+                [3,7,11],
+                [4,8,12],
+    ]
+
+
+
+boletoGanador=[]
+boletoGanadorAux=[]
+boletoGanadorNum=[]
+miBoleto=[]
+miBoletoAux=[]
+miBoletoAuxNum=[]
+aciertos=0
+estrellas=0
+
+
+print("Bienvenidos al sorteo de la loteria del EuroMillon del 3 de Diciembre de 2021 ")
+
+print("\n Para participar debe elegir 5 numeros de los primeros y 2 mas de la segunda")
+
+
+
+
+
+
+
+
+
+
